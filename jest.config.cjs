d571170d03a523d991ec8883364c4ef38cc2fb68
msdiniz <msdiniz@gmail.com>
2024-12-13 12:01:47 -0300

    all tests green

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,22 +1,20 @@
 // jest.config.cjs
-
 module.exports = {
   testEnvironment: 'jsdom',
   transform: {
     '^.+\\.tsx?$': ['ts-jest', {
-      tsconfig: 'tsconfig.json'
+      tsconfig: 'tsconfig.test.json'
     }]
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
   globals: {
     'ts-jest': {
-      tsconfig: 'tsconfig.json',
+      tsconfig: 'tsconfig.test.json',
     },
   },
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
     '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js',
   },
- // setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts', '@testing-library/jest-dom/extend-expect'],
 };
